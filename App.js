3c6e5863d406c9bf4326a8a74c1ccc936b46ecbf
Rafael Dias <rafael.dias@bemobi.com.br>
2022-01-19 00:01:28 -0300

    Adding redux. Creating store and root reducer

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 import MealsNavigator from "./navigation/MealsNavigator";
+import mealsReducer from "./store/reducers/meals";
+
 import { LogBox } from "react-native";
+
 LogBox.ignoreLogs(["It appears"]);
 
+const rootReducer = combineReducers({ meals: mealsReducer });
+const store = createStore(rootReducer);
+
 export default function App() {
-  
   let [fontsLoaded] = useFonts({
     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
     "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
@@ -16,5 +23,9 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <MealsNavigator />;
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
+  );
 }
